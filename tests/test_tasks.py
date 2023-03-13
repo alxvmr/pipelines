362f597d74eb7d0cf7d95d881714db078baa9541
alxvmr <alexeeva.maria16@gmail.com>
2023-03-13 23:38:43 +0400

    test files have been renamed

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -5,7 +5,7 @@ import pandas as pd
 from example_pipeline.pipeline import pipeline
 
 
-@pytest.mark.parametrize("table, output_file, correct_file", [("temp", "data\\temp_output.csv", "data\\temp_correct_load_file.csv")])
+@pytest.mark.parametrize("table, output_file, correct_file", [("temp", "data\\temp_output.csv", "data\\correct_load_file.csv")])
 def test_copy_to_file(create_table_for_CopyToFile, table, output_file, correct_file):
     task = CopyToFile(table, output_file)
     task.run()
@@ -18,7 +18,7 @@ def test_copy_to_file(create_table_for_CopyToFile, table, output_file, correct_f
     assert not (False in dif.values.reshape(-1))
     os.remove(output_file)
 
-@pytest.mark.parametrize("table, input_file", [("temp", "data\\temp_correct_load_file.csv")])
+@pytest.mark.parametrize("table, input_file", [("temp", "data\\correct_load_file.csv")])
 def test_load_file(input_file, table, client):
     task = LoadFile(table, input_file)
     task.run()
@@ -51,7 +51,7 @@ def test_ctas(table1, table2, query, create_table_for_ctas, client):
     assert client.number_of_records_after_query(query) == 1
     # проверим, что созданная таблица с таким же содержанием как у правильной
     out_table_file = "data\\temp1_output.csv"
-    table_cor = "data\\temp_correct_ctas.csv"
+    table_cor = "data\\correct_ctas.csv"
     client.copy_to_file(table2, out_table_file)
     f1 = pd.read_csv(out_table_file, sep=',')
     f2 = pd.read_csv(table_cor, sep=',')
@@ -67,7 +67,7 @@ def test_pipeline():
     os.chdir("..\\")
     pipeline.run()
     output_file = "example_pipeline\\data\\norm.csv"
-    cor_file = "data\\temp_correct_ctas.csv"
+    cor_file = "data/correct_ctas.csv"
     f1 = pd.read_csv(output_file, sep=',')
     os.chdir("tests")
     f2 = pd.read_csv(cor_file, sep=',')
